b739bbd121df634f97eeecce591d90e75b3d4404
JP <alias99002@outlook.com>
2023-09-27 13:57:54 -0500

    Metodology AAA Arrange, Act, Assert

--- demos/src/05-person.test.js
+++ demos/src/05-person.test.js
@@ -1,14 +1,22 @@
 const Person = require('./05-person');
-
+// AAA
+// Arrange / Given
+// Act / When
+// Assert / then
 describe('test for person', () => {
   let person;
+  // Arrange
+  // preparing an instace for every single test
   beforeEach(() => {
     person = new Person('Juan', 79, 1.82);
   });
 
   test('should return down', () => {
+    // Arrange
     person.weight = 79;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('normal');
   });
 
